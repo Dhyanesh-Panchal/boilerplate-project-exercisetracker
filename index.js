1ac00e9f7ee1d84b32ba01a3c22f7a3161b6ad0d
Dhyanesh Panchal <97329341+Dhyanesh-Panchal@users.noreply.github.com>
2023-03-13 14:45:58 +0000

    Post and Get request for Users working, corresponding tests passed

--- index.js
+++ index.js
@@ -20,12 +20,31 @@ const userSchema = new mongoose.Schema({
   }
 })
 
+const exerciseSchema = new mongoose.Schema({
+  user_id:{
+    type:String,
+    required:true  
+  },
+  description:{
+    type:String,
+    required:true
+  },
+  duration:{
+    type:Number,
+    required:true
+  },
+  date:{
+    type:Date,
+    required:true
+  }
+})
+
 let User = new mongoose.model('User', userSchema);
 
-const findUsers = async (name='') => {
+let Exercise = new mongoose.model('Exercise',exerciseSchema);
+const findUsers = async (name = '') => {
   //Request for all users
-  if(!name)
-  { 
+  if (!name) {
     let users = await User.find({});
     // console.log(users);
     return users;
@@ -34,10 +53,26 @@ const findUsers = async (name='') => {
   // console.log(users);
   return users;
 }
+
 const createUser = async (name) => {
   const newUser = await User.create({ username: name });
   newUser.save();
   return newUser;
+}
+
+const findExercise = async () => {
+  
+}
+
+const createExercise = async (user_id,description,duration,date) => {
+  const newExercise= await User.create({
+    user_id:user_id,
+    description:description,
+    duration:duration,
+    date: new Date(date)
+  })
+  console.log('new Exercise created');
+  return newExercise;
 }
 
 app.use(cors())
@@ -46,34 +81,43 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/users', async (req, res) => {
+  let allUsers = await findUsers();
+
+  console.log("Inside get request")
+  console.log(allUsers);
+  res.send(allUsers)
+})
+
 app.post('/api/users', bodyParser.urlencoded({ extended: false }), async (req, res) => {
   let postedUserName = req.body.username;
-  console.log(postedUserName);
-  
+  console.log("inside post request");
+  // console.log(postedUserName);
+
   //getting the user if exist
   let myUser = await findUsers(postedUserName);
-  
-  if (myUser.length==0) {
+
+  if (myUser.length == 0) {
     //No user exist. create one!
-    myUser=await createUser(postedUserName);
+    myUser = await createUser(postedUserName);
     console.log(myUser);
+  } else {
+    myUser = myUser[0];
   }
-  else {
-    console.log('user exist!');
-    console.log(myUser);
-  }
-  
+
   res.json({
     username: myUser.username,
     _id: myUser._id
   })
 });
 
-// app.get('/api/users',async (req,res)=>{
-//   let allUsers=await findUsers();
-//   // console.log(allUsers);
-//   res.send(allUsers)
-// })
+
+app.post('/api/users/:_id/exercises',bodyParser.urlencoded({ extended: false }),async (req,res)=>{
+  let postedData=req.body;
+  console.log(postedData[':_id']);
+  res.json(postedData);
+})
+
 
 
 
